248b813ed460c23c0b573c08642d22039be2f01c
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-10 17:38:51 +0530

    dependency errors changed

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@ termux-setup-storage
 pkg up -y
 pkg install python -y
 pkg install ffmpeg -y
-pip install -U yt-dlp
+pip install mutagen
+pip install --no-deps -U yt-dlp
 mv "YTD_Android.py" "/data/data/com.termux/files/home/ytdlp.py"
 cd "/data/data/com.termux/files/home/"
 echo ###Installation Done Successfully... Run the program with command "python ytdlp.py"###
