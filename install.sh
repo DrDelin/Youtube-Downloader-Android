a62d6e9f56993f5162c62a9c43b95caf577a781e
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-10 17:14:15 +0530

    end script error

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ pkg install python -y
 pkg install ffmpeg -y
 pip install --no-deps -U yt-dlp
 mv "YTD_Android.py" "/data/data/com.termux/files/home/ytdlp.py"
-cd
+cd "/data/data/com.termux/files/home/"
 echo ###Installation Done Successfully... Run the program with command "python ytdlp.py"###
