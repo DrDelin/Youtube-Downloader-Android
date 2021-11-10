4d7c34aede515adabbce14d224e446009517319a
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-10 16:18:13 +0530

    Little bit change in echo

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ pkg install python -y
 pkg install ffmpeg -y
 pip install yt-dlp
 mv "YTD_Android.py" "/data/data/com.termux/files/home/ytdlp.py"
-echo ###Installation Done Successfully... Run the program with command "python ytdlp.py"
+echo ###Installation Done Successfully... Run the program with command "python ytdlp.py"###
