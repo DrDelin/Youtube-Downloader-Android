60584517d212667ad53f0ac6d1a665c548de6c54
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-10 16:56:43 +0530

    install script improvement

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ termux-setup-storage
 pkg up -y
 pkg install python -y
 pkg install ffmpeg -y
-pip install yt-dlp
+pip install --no-deps -U yt-dlp
 mv "YTD_Android.py" "/data/data/com.termux/files/home/ytdlp.py"
+cd
 echo ###Installation Done Successfully... Run the program with command "python ytdlp.py"###
