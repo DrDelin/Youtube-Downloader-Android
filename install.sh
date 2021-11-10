a59627745d11d25c8afee4f18b93a98e55a5d199
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-10 17:28:25 +0530

    Dependency

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ termux-setup-storage
 pkg up -y
 pkg install python -y
 pkg install ffmpeg -y
-pip install --no-deps -U yt-dlp
+pip install -U yt-dlp
 mv "YTD_Android.py" "/data/data/com.termux/files/home/ytdlp.py"
 cd "/data/data/com.termux/files/home/"
 echo ###Installation Done Successfully... Run the program with command "python ytdlp.py"###
