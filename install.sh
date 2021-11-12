4b52f297c3d7384d26cd7cd0f535e8bcc9dde41d
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-12 15:46:37 +0530

    Small changes

--- install.sh
+++ install.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
 termux-setup-storage
 pkg up -y
+
 pkg install python -y
 pkg install ffmpeg -y
 pip install mutagen
 pip install --no-deps -U yt-dlp
+
 mv "YTD_Android.py" "/data/data/com.termux/files/home/ytdlp.py"
+mv "termux-url-opener" "/data/data/com.termux/files/home/termux-url-opener"
+
 cd "/data/data/com.termux/files/home/"
-echo ###Installation Done Successfully... Run the program with command "python ytdlp.py"###
+
+mkdir bin
+mv "termux-url-opener" "/data/data/com.termux/files/home/bin/termux-url-opener"
+
+cd bin
+chmod +x termux-url-opener
+
+echo ###Installation Done Successfully...Select Termux in share option of youtube app..."###
