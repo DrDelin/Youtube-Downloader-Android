b6ff59a332fd77aed201d12a849f7bb60423fb7e
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-13 17:11:43 +0530

    Changes in installation script

--- install.sh
+++ install.sh
@@ -7,8 +7,10 @@ pkg install python -y
 pkg install ffmpeg -y
 pip install mutagen
 pip install --no-deps -U yt-dlp
+pip3 install termux-apt-repo
+apt install transmission-gtk
 
-mv "YTD_Android.py" "/data/data/com.termux/files/home/ytdlp.py"
+mv "YTD_Android.py" "/data/data/com.termux/files/home/main.py"
 mv "termux-url-opener" "/data/data/com.termux/files/home/termux-url-opener"
 
 cd "/data/data/com.termux/files/home/"
