81bd3e82d94560f037f46fc408e87c94e271ce78
Dr_Delin <38278447+DrDelin@users.noreply.github.com>
2021-11-14 20:27:19 +0530

    Link fix 2

--- updater.py
+++ updater.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 #temp Download
-os.system(wget -P '/data/data/com.termux/files/home/' -q 'https://raw.githubusercontent.com/DrDelin/Youtube-Downloader-Android/master/YTD_Android.py')
+os.system(wget -P "/data/data/com.termux/files/home/" -q "https://raw.githubusercontent.com/DrDelin/Youtube-Downloader-Android/master/YTD_Android.py")
    
 #updator
 with open("/data/data/com.termux/files/home/YTD_Android.py") as u:
