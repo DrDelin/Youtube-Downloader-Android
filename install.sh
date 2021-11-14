6a0c9aa23ca403ebabddc01f8161e734162d23e7
Delin360 <38278447+DrDelin@users.noreply.github.com>
2021-11-14 13:50:30 +0530

    update

--- install.sh
+++ install.sh
@@ -9,10 +9,13 @@ pip install mutagen
 pip install --no-deps -U yt-dlp
 pip3 install termux-apt-repo
 pkg install x11-repo
+pkg install wget -y
 apt install transmission-gtk -y
 
 mv "YTD_Android.py" "/data/data/com.termux/files/home/main.py"
 mv "termux-url-opener" "/data/data/com.termux/files/home/termux-url-opener"
+mv "gversion.txt" "/data/data/com.termux/files/home/lversion.txt"
+rm "updatemanifest.sh"
 
 cd "/data/data/com.termux/files/home/"
 
