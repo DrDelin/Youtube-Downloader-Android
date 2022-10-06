6e50fe08b9817fd6911bb0799b07307de1d64de8
DrDelin <delin19@gmail.com>
2022-10-06 21:54:39 +0530

    History Bug fix

--- history.py
+++ history.py
@@ -10,20 +10,20 @@ with open(history, 'r+') as f:
         histlist.append(Dict)
 histlist2 = str(histlist).replace("'",'"')
 input_dict = json.loads(histlist2)
-
+print("\nHistory: \n")
 for i in input_dict:
-    print("\nHistory: \n")
     print(i["SNo"]+")", i["Name"]+"||", i["Site"])
 
 print("\nWhat to do!? \n")
 print("Select 1 to redownload from history: \nSelect 2 to clear history: \nSkip to close this script:\n")
-choice = input("Your Choice:\n")
+choice = input("Your Choice:")
 if choice=="1":
-    ask = input("Enter the SNo:\n")
+    ask = input("\nEnter the SNo:")
     output_dict = [a for a in input_dict if a["SNo"] == ask]
     for j in output_dict:
         print(j["SNo"]+")", j["Name"]+"||", j["Site"])
         url = j["URL"]
+        print("\n")
         os.system('python main.py "'+url+'"')
         exit()
 elif choice=="2":
