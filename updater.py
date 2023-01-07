78fb0bd9c1d74fcffe646a6cd06ee11988034599
Dr.Delin <delin19@gmail.com>
2023-01-08 01:23:45 +0530

    (Beta) Updater

--- updater.py
+++ updater.py
@@ -22,7 +22,28 @@ if os.path.isfile("/data/data/com.termux/files/home/YTD_Android.py"):
     m.close()
     u.close()
 else:
-    print("\nUpdate server busy or down!\nUpdate skipped...!")
-    code = "python '/data/data/com.termux/files/home/main.py' '" +sys.argv[1] +"'"
-    os.system(code)
+    print("New Mode")
+    url = "https://github.com/DrDelin/Youtube-Downloader-Android/blob/master/YTD_Android.py"
+    import requests
+    from bs4 import BeautifulSoup
+    r  = requests.get(url)
+    data = r.text
+    soup = BeautifulSoup(data, 'html.parser')
+    ver = soup.find(id="LC1")
+    update = ver.text.strip()
+    with open("/data/data/com.termux/files/home/main.py") as m:
+        main = m.readline().rstrip()
+    if update==main:
+        print("\nNo New Update...\n")
+        code = "python '/data/data/com.termux/files/home/main.py' '" +sys.argv[1] +"'"
+        os.system(code)
+    else:
+        print("\nUpdating......\n")
+        open('/data/data/com.termux/files/home/noobjection.temp', 'a').close()
+        os.system("sh refresh.sh")
+        code = "python '/data/data/com.termux/files/home/main.py' '" +sys.argv[1] +"'"
+        os.system(code)
+    m.close()
+    u.close()
+    
 quit()
