fc0807795fdebaa83f5ac36c2d8338eba6051cd1
Dr_Delin <38278447+DrDelin@users.noreply.github.com>
2023-03-09 14:46:57 +0530

    Regular Maintenance update

--- YTD_Android.py
+++ YTD_Android.py
@@ -1,5 +1,5 @@
 #Version 6.0.0.0
-#Engine 5.0
+#Engine 6.0
 
 #(Master) imports
 import os
@@ -19,7 +19,7 @@ linecache.clearcache()
 
 #Update news:
 print("(Changelog)Whats new...!\n")
-print("(Main)Complete rebase and optimizaion of code \n(Subtitle)Subtitle embeded with video \n(Download)Improved download speed and decreased code execution time \n(Patch)Empty folder remover \n(Engine) New Update and Upgrade Mechanism \n")
+print("(Engine)Binary update \n(Main)Complete rebase and optimizaion of code \n(Subtitle)Subtitle embeded with video \n(Download)Improved download speed and decreased code execution time \n(Patch)Empty folder remover \n(Engine) New Update and Upgrade Mechanism \n")
 
 #(Default) JSON file creation or verification:
 json_path = "/data/data/com.termux/files/home/default.json"
