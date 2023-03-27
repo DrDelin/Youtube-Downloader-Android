a993e94ee44800108d396b7c8a283e398b1a61a1
Senthil Manikandan <senthilmanikandan360@gmail.com>
2023-03-27 21:38:05 +0530

    Update based on release date

--- updater.py
+++ updater.py
@@ -1,1 +1,15 @@
 #ToDo - Build this updater script
+import subprocess
+from datetime import date
+from datetime import timedelta
+
+yesterday = date.today() - timedelta(days = 1)
+
+latest_release_date = str(subprocess.check_output('git log -1 --format=%ai ytd_win_beta', shell=True))
+latest_release_date = latest_release_date.split(' ')[0].split("\'")[1]
+latest_release_date = (latest_release_date.split('-')[0], latest_release_date.split('-')[1], latest_release_date.split('-')[2])
+
+new_release_date = date(
+    int(latest_release_date[0]), int(latest_release_date[1]), int(latest_release_date[2]))
+
+print("Latest release is newer : ", new_release_date > yesterday)
